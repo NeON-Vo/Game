a2cca236338b902ec6fda444b4147b8df0b50eb2
NeON-Vo <135706600+NeON-Vo@users.noreply.github.com>
2023-06-06 14:44:58 +0300

    Add files via upload

---
+++ game-bleach/data/items.js
@@ -0,0 +1,18 @@
+let itemsData = {
+    crystal: localStorage.getItem('crystal') || false,
+    vetki: localStorage.getItem('vetki') || false,
+    ruda: localStorage.getItem('ruda') || false,
+    koza: localStorage.getItem('koza') || false,
+    koshka: localStorage.getItem('koshka') || false,
+    komponents: localStorage.getItem('komponents') || false
+}
+
+let questData = {
+    kyznecIntro: localStorage.getItem('kyznecIntro') || false,
+    kyznecQuest: localStorage.getItem('kyznecQuest') || false,
+    stranijIntro: localStorage.getItem('stranijIntro') || false,
+    stranijQuest: localStorage.getItem('stranijQuest') || false,
+    koshatnikIntro: localStorage.getItem('koshatnikIntro') || false,
+    koshatnikQuest: localStorage.getItem('koshatnikQuest') || false
+    
+}
